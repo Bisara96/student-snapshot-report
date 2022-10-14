233f21b0c9031202a0121f29d3581ed69e48a4b8
Buwaneka96 <buwanekathilakasiri@gmail.com>
2022-10-14 23:32:14 +0530

    Added intial functionalities - configs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
+import { SnapshotReportComponent } from './snapshot-report/snapshot-report.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'report', component: SnapshotReportComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
+import { MaterialsModule } from './materials/materials.module';
+import { SnapshotReportModule } from './snapshot-report/snapshot-report.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialsModule,
+    MatTableModule,
+    SnapshotReportModule
   ],
   providers: [],
   bootstrap: [AppComponent]
